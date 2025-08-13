bf7630ed42be17b3614e6ee5d83c58d14f412569
RicoygLex <ricoyserglex@gmail.com>
2025-08-13 17:01:23 +0300

    First video completed

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+const app = express();
+app.get('/', (req, res) => {
+    let helloMessage = 'Hello World!';
+    res.send(helloMessage);
+});
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`);
+});
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import type { Request, Response } from 'express';
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+    let helloMessage = 'Hello World!';
+    res.send(helloMessage);
+});
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`);
+});
