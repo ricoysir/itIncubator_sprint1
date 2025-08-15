068a398df40e8432449ddde7dc00c90bdc89fda0
ricoysir <ricoysir2@gmail.com>
2025-08-15 18:07:06 +0300

    post queries basics learned

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 const app = express();
 const port = process.env.PORT || 3000;
 const addresses = [{ id: 1, value: 'Pushkina 1' }, { id: 2, value: 'Fillimonova 2' }];
 const products = [{ id: 1, title: 'tomato' }, { id: 2, title: 'apple' }];
+const parserMiddleware = bodyParser({});
+app.use(parserMiddleware);
 //products -------------------
 app.get('/products', (req, res) => {
     if (req.query.title) {
@@ -10,6 +13,21 @@ app.get('/products', (req, res) => {
         res.send(products.filter(p => p.title.indexOf(searchString) > -1));
     }
     res.send(products);
+});
+app.post('/products', (req, res) => {
+    const newProduct = {
+        id: products.length + 1,
+        title: req.body.title
+    };
+    const index = products.findIndex(p => p.id === newProduct.id);
+    if (index === -1) {
+        products.push(newProduct);
+        const newIndex = products.findIndex(p => p.id === newProduct.id);
+        res.sendStatus(201).send(newProduct);
+    }
+    else {
+        console.log(`"${newProduct.title}" already exists in the array.`);
+    }
 });
 app.get('/products/:id', (req, res) => {
     let searchedId = +req.params.id;
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import type { Request, Response } from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
 
@@ -7,6 +8,9 @@ const port = process.env.PORT || 3000;
 
 const addresses = [{ id: 1, value: 'Pushkina 1' }, { id: 2, value: 'Fillimonova 2' }]
 const products = [{ id: 1, title: 'tomato' }, { id: 2, title: 'apple' }]
+
+const parserMiddleware = bodyParser({})
+app.use(parserMiddleware);
 
 //products -------------------
 app.get('/products', (req: Request, res: Response) => {
@@ -18,10 +22,27 @@ app.get('/products', (req: Request, res: Response) => {
     res.send(products)
 })
 
+app.post('/products', (req: Request, res: Response) => {
+    const newProduct = {
+        id: products.length + 1,
+        title: req.body.title
+    }
+    const index = products.findIndex(p => p.id === newProduct.id);
+
+    if (index === -1) {
+        products.push(newProduct);
+        const newIndex = products.findIndex(p => p.id === newProduct.id);
+        res.sendStatus(201).send(newProduct);
+    } else {
+        console.log(`"${newProduct.title}" already exists in the array.`);
+    }
+})
+
+
 app.get('/products/:id', (req: Request, res: Response) => {
     let searchedId = +req.params.id;
     let product = products.find(p => p.id === searchedId);
-    if(!product){
+    if (!product) {
         res.sendStatus(404);
     }
     res.send(product)
@@ -29,10 +50,8 @@ app.get('/products/:id', (req: Request, res: Response) => {
 
 app.delete('/products/:id', (req: Request, res: Response) => {
     let searchedId = +req.params.id;
-    for (let i = 0; i < products.length; i++,)
-    {
-        if (products[i].id === searchedId)
-        {
+    for (let i = 0; i < products.length; i++,) {
+        if (products[i].id === searchedId) {
             products.splice(i, 1);
             res.sendStatus(204);
             return;
@@ -48,7 +67,7 @@ app.get('/addresses', (req: Request, res: Response) => {
 app.get('/addresses/:id', (req: Request, res: Response) => {
     let searchedId = +req.params.id;
     let address = addresses.find(p => p.id === searchedId);
-    if(!address){
+    if (!address) {
         res.sendStatus(404);
     }
     res.send(address)
