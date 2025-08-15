bcbefe0e40c445cad2a6e426f9008e4b9a280e86
ricoysir <ricoysir2@gmail.com>
2025-08-15 16:34:21 +0300

    get basis learned

--- dist/index.js
+++ dist/index.js
@@ -1,17 +1,33 @@
 import express from 'express';
 const app = express();
 const port = process.env.PORT || 3000;
-const addresses = [{ value: 'Pushkina 1' }, { value: 'Fillimonova 2' }];
-const products = [{ title: 'tomato' }, { title: 'apple' }];
+const addresses = [{ id: 1, value: 'Pushkina 1' }, { id: 2, value: 'Fillimonova 2' }];
+const products = [{ id: 1, title: 'tomato' }, { id: 2, title: 'apple' }];
 app.get('/products', (req, res) => {
+    if (req.query.title) {
+        let searchString = req.query.title?.toString();
+        res.send(products.filter(p => p.title.indexOf(searchString) > -1));
+    }
     res.send(products);
 });
-app.get('/products/tomato', (req, res) => {
-    let tomato = products.find(p => p.title === 'tomato');
-    res.send(tomato);
+app.get('/products/:id', (req, res) => {
+    let searchedId = +req.params.id;
+    let product = products.find(p => p.id === searchedId);
+    if (!product) {
+        res.sendStatus(404);
+    }
+    res.send(product);
 });
 app.get('/addresses', (req, res) => {
     res.send(addresses);
+});
+app.get('/addresses/:id', (req, res) => {
+    let searchedId = +req.params.id;
+    let address = addresses.find(p => p.id === searchedId);
+    if (!address) {
+        res.sendStatus(404);
+    }
+    res.send(address);
 });
 app.get('/', (req, res) => {
     let helloMessage = 'Hello Incubator!';
--- src/index.ts
+++ src/index.ts
@@ -5,20 +5,37 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-const addresses = [{value: 'Pushkina 1'}, {value: 'Fillimonova 2'}]
-const products = [{title: 'tomato'}, {title: 'apple'}]
+const addresses = [{ id: 1, value: 'Pushkina 1' }, { id: 2, value: 'Fillimonova 2' }]
+const products = [{ id: 1, title: 'tomato' }, { id: 2, title: 'apple' }]
 
 app.get('/products', (req: Request, res: Response) => {
+    if (req.query.title) {
+        let searchString = req.query.title?.toString();
+        res.send(products.filter(p => p.title.indexOf(searchString) > -1)
+        )
+    }
     res.send(products)
 })
- 
-app.get('/products/tomato', (req: Request, res: Response) => {
-    let tomato = products.find(p => p.title === 'tomato');
-    res.send(tomato)
+
+app.get('/products/:id', (req: Request, res: Response) => {
+    let searchedId = +req.params.id;
+    let product = products.find(p => p.id === searchedId);
+    if(!product){
+        res.sendStatus(404);
+    }
+    res.send(product)
 })
 
 app.get('/addresses', (req: Request, res: Response) => {
     res.send(addresses)
+})
+app.get('/addresses/:id', (req: Request, res: Response) => {
+    let searchedId = +req.params.id;
+    let address = addresses.find(p => p.id === searchedId);
+    if(!address){
+        res.sendStatus(404);
+    }
+    res.send(address)
 })
 
 app.get('/', (req: Request, res: Response) => {
