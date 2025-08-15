034add6c99b7322ab926b139c73285a9dc220493
ricoysir <ricoysir2@gmail.com>
2025-08-15 16:04:47 +0300

    lesson 2. Express and RestAPI

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,22 @@
 import express from 'express';
 const app = express();
+const port = process.env.PORT || 3000;
+const addresses = [{ value: 'Pushkina 1' }, { value: 'Fillimonova 2' }];
+const products = [{ title: 'tomato' }, { title: 'apple' }];
+app.get('/products', (req, res) => {
+    res.send(products);
+});
+app.get('/products/tomato', (req, res) => {
+    let tomato = products.find(p => p.title === 'tomato');
+    res.send(tomato);
+});
+app.get('/addresses', (req, res) => {
+    res.send(addresses);
+});
 app.get('/', (req, res) => {
     let helloMessage = 'Hello Incubator!';
     res.send(helloMessage);
 });
-const port = 3000;
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`);
 });
--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,29 @@ import type { Request, Response } from 'express';
 
 const app = express();
 
+const port = process.env.PORT || 3000;
+
+const addresses = [{value: 'Pushkina 1'}, {value: 'Fillimonova 2'}]
+const products = [{title: 'tomato'}, {title: 'apple'}]
+
+app.get('/products', (req: Request, res: Response) => {
+    res.send(products)
+})
+ 
+app.get('/products/tomato', (req: Request, res: Response) => {
+    let tomato = products.find(p => p.title === 'tomato');
+    res.send(tomato)
+})
+
+app.get('/addresses', (req: Request, res: Response) => {
+    res.send(addresses)
+})
+
 app.get('/', (req: Request, res: Response) => {
     let helloMessage = 'Hello Incubator!';
     res.send(helloMessage);
 });
 
-const port = 3000;
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`);
 });
